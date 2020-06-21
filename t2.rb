8bda7881a4faae53bfc74102b69c81dba73a8462
Jose Hidago <jhidalgo521@gmail.com>
2020-06-21 15:24:31 +0000

    Automatically backed up by Learn

---
+++ t2.rb
@@ -0,0 +1,19 @@
+# write method reverse_each_word that takes sentence
+# takes words of that sentence and reverses them in place
+# use .each 
+require 'pry'
+#trial 1 
+
+s = "hello there, and how are you?"
+
+def rew(s)
+  new = []
+  s.each{|w| w.reverse}
+  binding.pry
+  s
+end
+
+rew(s)
+
+#code didnt run .each cant be used on string. Can only be used on collections like hashes or arrays
+# solution could be to create an array out of the each word of the sentence
