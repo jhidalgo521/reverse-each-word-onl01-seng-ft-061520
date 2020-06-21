029fd63fb0f02c2411bf4a3f13501acfb94f623c
Jose Hidago <jhidalgo521@gmail.com>
2020-06-21 15:08:40 +0000

    Automatically backed up by Learn

---
+++ t1.rb
@@ -0,0 +1,13 @@
+# write method reverse_each_word that takes sentence
+# takes words of that sentence and reverses them in place
+# use .each 
+
+#trial 1 
+
+s = "hello there, and how are you?"
+
+def rew(s)
+  s.each.reverse do |word|
+  end
+  s
+end
